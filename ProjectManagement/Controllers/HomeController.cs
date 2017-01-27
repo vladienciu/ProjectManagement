34e91f9e7d231124471c74a7dc3f4e4aca131ec1
Vlad Ienciu <Vlad Ienciu>
2017-01-27 21:40:17 +0200

    MVC Course 5

---
+++ ProjectManagement/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            ViewBag.Author = "Vlad Ienciu";
+
+            var model = new Models.AboutModel();
+            model.Message = "This is a test";
+            model.Author = "Vlad";
+            return View(model);
+
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ ProjectManagement/Models/AboutModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectManagement.Models
+{
+    public class AboutModel
+    {
+        public string Message { get; set; }
+
+        public string Author { get; set; }
+    }
+}
